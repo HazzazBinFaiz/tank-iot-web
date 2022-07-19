64beda31ab125f95356093cb29e7791a15d9515c
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-07-19 10:43:58 +0600

    Wip admin from middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,9 +14,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if(! $request->expectsJson() && $request->is('admin*')) {
-            return route('admin.login');
-        }
         if (! $request->expectsJson()) {
             return route('login');
         }
