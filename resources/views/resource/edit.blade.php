3d2d4611975507a69be3fd0ccfe45050c231b950
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-07-20 10:57:09 +0600

    Create dynamic resource

---
+++ resources/views/resource/edit.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout :title="__('Edit '.Str::studly($name))">
+    <x-slot name="header">
+        <div class="w-full flex justify-between">
+            <div class="text-xl">{{ __('Edit '.Str::of($name)->studly()->plural()) }}</div>
+            @if(Route::has($name.'.index'))
+                @can($name.'-read')
+                    <div>
+                        <a
+                            href="{{ route($name.'.index') }}"
+                            class="bg-transparent text-sm hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-4 border border-blue-500 hover:border-transparent rounded"
+                        >{{ __((string) Str::of($name)->studly()->plural()) }}</a>
+                    </div>
+                @endcan
+            @endif
+        </div>
+    </x-slot>
+
+
+    <form action="{{ route($name.'.update', $model->id) }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+        <div class="flex flex-wrap justify-center w-full bg-white p-4">
+            @foreach($fields as $field)
+                @if($field->type === 'text')
+                    <x-labeled-input :name="$field->name" :required="$field->required" :value="$model->{$field->name}" class="w-full p-1 md:w-1/2"/>
+                @elseif($field->type === 'select')
+                    <x-labeled-select :name="$field->name" :required="$field->required" class="w-full p-1 md:w-1/2">
+                        @foreach($field->options as $value => $label)
+                            <option @if(old($field->name, $model->{$field->name} instanceof \BenSampo\Enum\Enum ? $model->{$field->name}->value : $model->{$field->name}) == $value) selected
+                                    @endif value="{{ $value }}">{{ $label }}</option>
+                        @endforeach
+                    </x-labeled-select>
+                @endif
+            @endforeach
+            <div class="w-full pt-8 py-4 flex justify-center">
+                <x-button>{{ __('Update') }}</x-button>
+            </div>
+        </div>
+    </form>
+</x-app-layout>
